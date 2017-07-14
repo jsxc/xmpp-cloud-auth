264895c7ccbbfdaab45edec8e6757c1a2ac42ccb
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2017-07-14 15:02:03 +0200

    Fixed permissions in install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 adduser --system --group --home /var/cache/xcauth --gecos "XMPP Cloud Authentication" xcauth
-mkdir --mode 640 /var/log/xcauth
+# Add group xcauth to users prosody, ejabberd, if they exist
+groups prosody > /dev/null 2>&1 && adduser prosody xcauth
+groups ejabberd > /dev/null 2>&1 && adduser ejabberd xcauth
+mkdir /var/log/xcauth
+chmod 770 /var/{log,cache}/xcauth
 chown xcauth:xcauth /var/log/xcauth
