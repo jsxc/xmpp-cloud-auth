b8c76aee2926441f29cc4487290ca17e9e85d06c
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2017-07-21 09:05:43 +0000

    Use /var/{lib,log,cache}/xcauth directories

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ adduser --system --group --home /var/cache/xcauth --gecos "XMPP Cloud Authentica
 # Add group xcauth to users prosody, ejabberd, if they exist
 groups prosody > /dev/null 2>&1 && adduser prosody xcauth
 groups ejabberd > /dev/null 2>&1 && adduser ejabberd xcauth
-mkdir /var/log/xcauth
-chmod 770 /var/{log,cache}/xcauth
-chown xcauth:xcauth /var/log/xcauth
+mkdir -p /var/{log,lib,cache}/xcauth
+chmod 770 /var/{log,lib,cache}/xcauth
+chown xcauth:xcauth /var/{log,lib,cache}/xcauth
