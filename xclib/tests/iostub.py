be2e01fa3c94d8bd526338495feef406f571380e
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2017-08-18 09:17:32 +0200

    Indent=4

--- xclib/tests/iostub.py
+++ xclib/tests/iostub.py
@@ -2,32 +2,32 @@ import sys
 import io
 
 class iostub:
-  def stub_stdin(testcase_inst, inputs):
-    stdin = sys.stdin
+    def stub_stdin(testcase_inst, inputs):
+        stdin = sys.stdin
 
-    def cleanup():
-      sys.stdin = stdin
+        def cleanup():
+            sys.stdin = stdin
 
-    testcase_inst.addCleanup(cleanup)
-    sys.stdin = io.BytesIO(inputs)
+        testcase_inst.addCleanup(cleanup)
+        sys.stdin = io.BytesIO(inputs)
 
-  def stub_stdout(testcase_inst):
-    stdout = sys.stdout
+    def stub_stdout(testcase_inst):
+        stdout = sys.stdout
 
-    def cleanup():
-      sys.stdout = stdout
+        def cleanup():
+            sys.stdout = stdout
 
-    testcase_inst.addCleanup(cleanup)
-    sys.stdout = io.BytesIO()
+        testcase_inst.addCleanup(cleanup)
+        sys.stdout = io.BytesIO()
 
-  def stub_stdouts(testcase_inst):
-    stderr = sys.stderr
-    stdout = sys.stdout
+    def stub_stdouts(testcase_inst):
+        stderr = sys.stderr
+        stdout = sys.stdout
 
-    def cleanup():
-      sys.stderr = stderr
-      sys.stdout = stdout
+        def cleanup():
+            sys.stderr = stderr
+            sys.stdout = stdout
 
-    testcase_inst.addCleanup(cleanup)
-    sys.stderr = io.BytesIO()
-    sys.stdout = io.BytesIO()
+        testcase_inst.addCleanup(cleanup)
+        sys.stderr = io.BytesIO()
+        sys.stdout = io.BytesIO()
--- xclib/tests/prosody_test.py
+++ xclib/tests/prosody_test.py
@@ -6,27 +6,27 @@ from xclib.tests.iostub import iostub
 
 class TestProsody(unittest.TestCase, iostub):
 
-  def test_input(self):
-    self.stub_stdin('isuser:login:\n' +
-      'auth:log:dom:pass\n')
-    tester = iter(prosody_io.read_request())
-    output = tester.next()
-    assert output == ('isuser', 'login', '')
-    output = tester.next()
-    assert output == ('auth', 'log', 'dom', 'pass')
-    try:
-      output = tester.next()
-      assert False # Should raise StopIteration
-    except StopIteration:
-      pass
+    def test_input(self):
+        self.stub_stdin('isuser:login:\n' +
+            'auth:log:dom:pass\n')
+        tester = iter(prosody_io.read_request())
+        output = tester.next()
+        assert output == ('isuser', 'login', '')
+        output = tester.next()
+        assert output == ('auth', 'log', 'dom', 'pass')
+        try:
+            output = tester.next()
+            assert False # Should raise StopIteration
+        except StopIteration:
+            pass
 
-  def test_output_false(self):
-    self.stub_stdout()
-    prosody_io.write_response(False)
-    self.assertEqual(sys.stdout.getvalue(), '0\n')
+    def test_output_false(self):
+        self.stub_stdout()
+        prosody_io.write_response(False)
+        self.assertEqual(sys.stdout.getvalue(), '0\n')
 
-  # Cannot be merged, as getvalue() returns the aggregate value
-  def test_output_true(self):
-    self.stub_stdout()
-    prosody_io.write_response(True)
-    self.assertEqual(sys.stdout.getvalue(), '1\n')
+    # Cannot be merged, as getvalue() returns the aggregate value
+    def test_output_true(self):
+        self.stub_stdout()
+        prosody_io.write_response(True)
+        self.assertEqual(sys.stdout.getvalue(), '1\n')
