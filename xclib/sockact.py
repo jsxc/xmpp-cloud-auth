8e1d39d1d57c67c09d163a0bf07c39f6c67b31e7
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2018-07-22 15:40:36 +0200

    Compatible with Python <3.6; mention conflict with python-systemd

--- xclib/sockact.py
+++ xclib/sockact.py
@@ -4,24 +4,27 @@ import os
 import logging
 
 def listen_fds_with_names():
+    '''Tries to get file descriptors from (in order):
+    - listen_fds_with_names() # Not yet merged, see https://github.com/systemd/python-systemd/pull/60
+    - listen_fds() # With own hack in case it finds $LISTEN_FDNAMES until listen_fds_with_names() is supported
+    - None
+    In the first two cases, it returns a hash of {fd: name} pairs'''
     try:
         from systemd.daemon import listen_fds_with_names
         # We have the real McCoy
         return listen_fds_with_names()
-    except ModuleNotFoundError: # Inherits from ImportError, needs thus to be first
-        # Not yet there in Python 3.5
-        # No systemd.daemon found; try to fail appropriately:
-        # - If $LISTEN_FDS is set, then insist on having systemd.daemon
-        # - Else be lenient and remain compatible with pre-systemd users
-        if os.path.exists('/run/systemd/system') and 'LISTEN_FDS' in os.environ:
-            logging.error('Software from https://github.com/systemd/python-systemd/ missing; do `apt install python3-systemd` or `pip3 install systemd-python` (please note the similarly-named `pip3 install python-systemd` does not provide the interfaces needed)')
-            raise
-        else:
-            logging.info('Please `apt install python3-systemd` for future compatibility')
     except ImportError:
         # Try to fall back to listen_fds(),
         # possbily emulating listen_fds_with_names() here
-        from systemd.daemon import listen_fds
+        try:
+            from systemd.daemon import listen_fds
+        except ImportError:
+            if os.path.exists('/run/systemd/system') and 'LISTEN_FDS' in os.environ:
+                logging.error('Software from https://github.com/systemd/python-systemd/ missing; do `apt install python3-systemd` or `pip3 install systemd-python` (please note the similarly-named `pip3 install python-systemd` does not provide the interfaces needed). Please note it is incompatible with `pip3 install systemd-python`!')
+                raise
+            else:
+                logging.info('Please `apt install python3-systemd` for future compatibility')
+                return None
         fds = listen_fds()
         if fds:
             listeners = {}
