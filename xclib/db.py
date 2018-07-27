97a11932f3fcf8105e2dc15d064772e6948d2d5a
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2018-07-27 20:51:22 +0200

    Migration

--- xclib/db.py
+++ xclib/db.py
@@ -1,77 +1,103 @@
+import bsddb3
 import sqlite3
-
+from xclib.utf8 import unutf8
 
 class connection:
     def __init__(self, paths):
         self.paths = paths
 
     def __enter__(self):
-        self.conn = sqlite3.connect(self.paths.db)
-        # Add bound method execute_check to this instance
-        self.conn.execute_check = self.execute_check.__get__(self)
+        self.conn = sqlite3.connect(self.paths.db, factory=sconn)
+        self.conn.set_paths(self.paths)
         return self.conn
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, traceback):
         self.conn.close()
+        return False # Do not suppress exceptions
 
-    def execute_check(self, *args, **kwargs):
+class sconn(sqlite3.Connection):
+    def set_paths(self, paths):
+        self.paths = paths
+        self.upgrade = False
+
+    def execute(self, *args, **kwargs):
         '''If the schema is not yet there, create it and fill the database first'''
-        try:
-            self.conn.execute(*args, **kwargs)
-        except sqlite3.OperationalError, e:
-            # Convert and try again
-            self.db_upgrade()
-            self.conn.execute(*args, **kwargs)
+        if self.upgrade:
+            # Avoid recursion problems on upgrade errors (or after upgrading)
+            return super().execute(*args, **kwargs)
+        else:
+            try:
+                return super().execute(*args, **kwargs)
+            except sqlite3.OperationalError as e:
+                # Convert and try again
+                self.upgrade = True
+                self.db_upgrade()
+                return super().execute(*args, **kwargs)
 
     def db_upgrade(self):
         self.db_upgrade_domain()
         self.db_upgrade_cache()
         self.db_upgrade_roster()
 
-    def db_upgrade_domain():
-        self.conn.execute('''CREATE TABLE domains (xmppdomain TEXT PRIMARY KEY,
+    def db_upgrade_domain(self):
+        self.execute('''CREATE TABLE domains
+                     (xmppdomain TEXT PRIMARY KEY,
                       authsecret TEXT,
                       authurl    TEXT,
                       authdomain TEXT,
                       regcontact TEXT,
-                      regfirst   TEXT DEFAULT datetime('now'),
-                      reglatest  TEXT DEFAULT datetime('now'))''')
-        self.conn.execute('BEGIN TRANSACTION')
+                      regfirst   TEXT DEFAULT CURRENT_TIMESTAMP,
+                      reglatest  TEXT DEFAULT CURRENT_TIMESTAMP)''')
+        self.execute('BEGIN TRANSACTION')
         try:
-            db = anydbm.open(self.paths.domain_db, 'r')
-            for k,v in db.iteritems():
-                (authsecret, authurl, authdomain, extra) = v.split("\t", 3)
-                self.conn.execute('''INSERT INTO domains (xmppdomain, authsecret, authurl, authdomain)
+            db = bsddb3.hashopen(self.paths.domain_db, 'r')
+            for k,v in db.items():
+                k = unutf8(k)
+                v = unutf8(v)
+                try:
+                    (authsecret, authurl, authdomain, extra) = v.split("\t", 3)
+                except ValueError:
+                    (authsecret, authurl, authdomain) = v.split("\t", 2)
+                    extra = None
+                self.execute('''INSERT INTO domains (xmppdomain, authsecret, authurl, authdomain)
                      VALUES (?, ?, ?, ?)''', (k, authsecret, authurl, authdomain))
             db.close()
-        except anydbm.error, e:
+        except bsddb3.db.DBError as e:
             pass
-        self.conn.execute('COMMIT')
+        self.execute('COMMIT')
 
-    def db_upgrade_cache():
-        self.conn.execute('''CREATE TABLE authcache (jid        TEXT PRIMARY KEY,
+    def db_upgrade_cache(self):
+        self.execute('''CREATE TABLE authcache
+                       (jid        TEXT PRIMARY KEY,
                         pwhash     TEXT,
-                        firstauth  TEXT DEFAULT datetime('now'),
-                        anyauth    TEXT DEFAULT datetime('now'),
-                        remoteauth TEXT DEFAULT datetime('now'))''')
-        self.conn.execute('BEGIN TRANSACTION')
+                        firstauth  TEXT DEFAULT CURRENT_TIMESTAMP,
+                        anyauth    TEXT DEFAULT CURRENT_TIMESTAMP,
+                        remoteauth TEXT DEFAULT CURRENT_TIMESTAMP)''')
+        self.execute('BEGIN TRANSACTION')
         try:
-            db = anydbm.open(self.paths.cache_db, 'r')
-            for k,v in db.iteritems():
+            db = bsddb3.hashopen(self.paths.cache_db, 'r')
+            for k,v in db.items():
+                k = unutf8(k)
+                v = unutf8(v)
                 (pwhash, ts1, tsv, tsa, rest) = v.split("\t", 4)
-                self.conn.execute('''INSERT INTO authcache (jid, pwhash, firstauth, anyauth, remoteauth)
-                     VALUES (?, ?, ?, ?, ?)''', (k, pwhash, ts1, tsv, tsa))
+                self.execute('''INSERT INTO authcache (jid, pwhash, firstauth, anyauth, remoteauth)
+                     VALUES (?, ?,
+                     datetime('unixepoch' || ?),
+                     datetime('unixepoch' || ?),
+                     datetime('unixepoch' || ?))''', (k, pwhash, ts1, tsv, tsa))
             db.close()
-        except anydbm.error, e:
+        except bsddb3.db.DBError as e:
             pass
-        self.conn.execute('COMMIT')
+        self.execute('COMMIT')
 
-    def db_upgrade_roster():
-        self.conn.execute('''CREATE TABLE rosterinfo   (jid          TEXT PRIMARY KEY,
+    def db_upgrade_roster(self):
+        self.execute('''CREATE TABLE rosterinfo
+                          (jid          TEXT PRIMARY KEY,
                            fullname     TEXT,
                            grouplist    TEXT,
                            responsehash TEXT)''')
-        self.conn.execute('''CREATE TABLE rostergroups (groupname    TEXT PRIMARY KEY,
+        self.execute('''CREATE TABLE rostergroups
+                          (groupname    TEXT PRIMARY KEY,
                            userlist     TEXT)''')
         rosterinfo_fn = {}
         rosterinfo_rh = {}
@@ -80,8 +106,10 @@ class connection:
         rosterusers = set([])
         rostergroups = {}
         try:
-            db = anydbm.open(self.paths.cache_db, 'r')
-            for k,v in db.iteritems():
+            db = bsddb3.hashopen(self.paths.cache_db, 'r')
+            for k,v in db.items():
+                k = unutf8(k)
+                v = unutf8(v)
                 if k[:4] == 'FNC:': # Full name (cache only)
                     jid = k[4:].replace(':', '@')
                     rosterusers = rosterusers + jid
@@ -99,13 +127,15 @@ class connection:
                     rosterusers = rosterusers + jid
                     rosterinfo_rc[jid] = v
             db.close()
-        except anydbm.error, e:
+        except bsddb3.db.DBError as e:
             pass
 
         rg = []
-        for k,v in rostergroups.iteritems():
+        for k,v in rostergroups.items():
+            k = unutf8(k)
+            v = unutf8(v)
             rg.append([k,v])
-        self.conn.executemany('INSERT INTO rostergroups (groupname, userlist) VALUES (?, ?)', rg)
+        self.executemany('INSERT INTO rostergroups (groupname, userlist) VALUES (?, ?)', rg)
 
         ri = []
         for k in rosterusers:
@@ -113,5 +143,5 @@ class connection:
                 rosterinfo_fn[k] if k in rosterinfo_fn else None,
                 rosterinfo_lg[k] if k in rosterinfo_lg else None,
                 rosterinfo_rh[k] if k in rosterinfo_rh else None])
-        self.conn.executemany('INSERT INTO rosterinfo (jid, fullname, grouplist, responsehash) VALUES (?, ?, ?, ?)', ri)
+        self.executemany('INSERT INTO rosterinfo (jid, fullname, grouplist, responsehash) VALUES (?, ?, ?, ?)', ri)
 
---
+++ xclib/tests/14_db_migration_test.py
@@ -0,0 +1,64 @@
+# Checks database migration functions
+import sys
+import os
+import io
+import bsddb3
+import unittest
+import tempfile
+import logging
+import shutil
+from argparse import Namespace
+from xclib.dbmops import perform
+from xclib.tests.iostub import iostub
+from xclib.utf8 import utf8
+from xclib.check import assertEqual
+from xclib.db import connection
+
+class TestDBM(unittest.TestCase, iostub):
+
+    @classmethod
+    def setup_class(cls):
+        global domname, cacname, rosname, sqlname, domfile, dirname
+        dirname = tempfile.mkdtemp()
+        domname = dirname + "/domains.db"
+        rosname = dirname + "/shared_roster.db"
+        ucdname = dirname + "/user-cache.db"
+        sqlname = dirname + "/xcauth.sqlite3"
+        domfile = bsddb3.hashopen(domname, 'c', 0o600)
+
+    @classmethod
+    def teardown_class(cls):
+        domfile.close()
+        shutil.rmtree(dirname)
+
+    def mkns(self, **kwargs):
+        params = {'domain_db': domname, 'get': None, 'put': None, 
+            'delete': None, 'load': None, 'unload': None}
+        params.update(**kwargs)
+        return Namespace(**params)
+
+    def mkpaths(self, **kwargs):
+        paths = {'db': sqlname, 'domain_db': domname, 'cache_db': rosname}
+        paths.update(**kwargs)
+        return Namespace(**paths)
+
+    def test_01_load(self):
+        self.stub_stdin(u'example.ch\tXmplScrt\thttps://example.ch/index.php/apps/ojsxc/ajax/externalApi.php\texample.ch\t\n' +
+            u'example.de\tNothrXampl\thttps://nothing\t\n')
+        ns = self.mkns(load=True)
+        perform(ns)
+        assertEqual(domfile[b'example.ch'], b'XmplScrt\thttps://example.ch/index.php/apps/ojsxc/ajax/externalApi.php\texample.ch\t')
+        assertEqual(domfile[b'example.de'], b'NothrXampl\thttps://nothing\t')
+        assert b'example.net' not in domfile
+
+    def test_02_execute(self):
+        paths = self.mkpaths()
+        sqlconn = connection(paths)
+        r = set()
+        with sqlconn as c:
+            for row in c.execute('select * from domains'):
+                r.add(row[0:4])
+        logging.error('r = %r', r)
+        assertEqual(len(r), 2)
+        assert ('example.ch', 'XmplScrt', 'https://example.ch/index.php/apps/ojsxc/ajax/externalApi.php', 'example.ch') in r
+        assert ('example.de', 'NothrXampl', 'https://nothing', '') in r
