4e5ef4a04fd0b3b354b187ee6e8e8ef27337ad6f
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2018-08-05 22:33:03 +0200

    Allow dumping illegal utf-8 contents

--- xclib/dbmops.py
+++ xclib/dbmops.py
@@ -5,14 +5,14 @@ from xclib.utf8 import utf8, unutf8
 def perform(args):
     domain_db = bsddb3.hashopen(args.domain_db, 'c', 0o600)
     if args.get:
-        print(unutf8(domain_db[utf8(args.get)]))
+        print(unutf8(domain_db[utf8(args.get)], 'illegal'))
     elif args.put:
         domain_db[utf8(args.put[0])] = args.put[1]
     elif args.delete:
         del domain_db[utf8(args.delete)]
     elif args.unload:
         for k in list(domain_db.keys()):
-            print('%s\t%s' % (unutf8(k), unutf8(domain_db[k])))
+            print('%s\t%s' % (unutf8(k, 'illegal'), unutf8(domain_db[k], 'illegal')))
         # Should work according to documentation, but doesn't
         # for k, v in DOMAIN_DB.iteritems():
         #     print k, '\t', v
