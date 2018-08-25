e052723e1e89ae929167133e0588b616e684b2cb
Marcel Waldvogel <marcel.waldvogel@uni-konstanz.de>
2018-08-25 15:42:25 +0200

    Fix permissions as part of restart
    This is one of the most common mistakes:
    
    - The first `xcauth` run is as root, creating the files as root
    - Following daemon runs as user "xcauth" cannot access DB and
      log files
    
    This avoids this problem.

--- tools/xcrestart.sh
+++ tools/xcrestart.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+echo "Fixing permissions (just in case)"
+chown -R xcauth:xcauth /etc/xcauth.conf /var/*/xcauth
 echo "Stopping xcauth.service, if running"
 systemctl -q stop xcauth.service
 echo "(Re-)starting listening sockets"
